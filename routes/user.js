84a5dd8b55c69846cec99838dbe7a311203d4e10
Olga Dokukova <dokukova@gmail.com>
2019-11-02 12:59:38 +0100

    wip

--- routes/user.js
+++ routes/user.js
@@ -8,15 +8,15 @@ const router = express.Router();
 const { checkIfLoggedIn } = require('../middlewares');
 
 // GET show user's profile
-router.get('/profile', checkIfLoggedIn, async (req, res, next) => {
-  const { _id } = req.session.currentUser;
-  try {
-    const user = await User.findById({ _id });
-    res.json(user);
-  } catch (error) {
-    next(error);
-  }
-});
+// router.get('/profile', checkIfLoggedIn, async (req, res, next) => {
+//   const { _id } = req.session.currentUser;
+//   try {
+//     const user = await User.findById({ _id });
+//     res.json(user);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 // GET show user's collection
 router.get('/mycollection', checkIfLoggedIn, async (req, res, next) => {
@@ -31,13 +31,13 @@ router.get('/mycollection', checkIfLoggedIn, async (req, res, next) => {
 
 // GET show user's wishlist
 router.get('/mywishlist', checkIfLoggedIn, async (req, res, next) => {
-    const { _id } = req.session.currentUser;
-    try {
-      const user = await User.findById({ _id });
-      res.json(user.myWishlist);
-    } catch (error) {
-      next(error);
-    }
-  });
+  const { _id } = req.session.currentUser;
+  try {
+    const user = await User.findById({ _id });
+    res.json(user.myWishlist);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
