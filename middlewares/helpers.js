7395498d3c352e34684448970f49ae84328b38eb
Olga Dokukova <dokukova@gmail.com>
2019-11-10 00:12:16 +0100

    wip

--- middlewares/helpers.js
+++ middlewares/helpers.js
@@ -1,5 +1,5 @@
+const express = require('express');
 const MyDoll = require('../models/MyDoll');
-
 const router = express.Router();
 
 const { checkIfLoggedIn } = require('./index');
--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
 const { getList, getSingleDoll, addDollToList, updateDoll, deleteDoll } = require('../middlewares/helpers');
 
 const myCollection = 'mycollection';
