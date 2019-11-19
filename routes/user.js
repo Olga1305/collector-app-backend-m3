036aa488edc5d4bc6db4b72268b453b46cbc3b78
Olga Dokukova <dokukova@gmail.com>
2019-11-19 05:28:14 +0100

    [feat] close #37 search by query route

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../models/User");
+const Doll = require("../models/Doll");
 const MyDoll = require("../models/MyDoll");
 
 const router = express.Router();
@@ -280,5 +281,33 @@ router.get(
     }
   }
 );
+
+// GET dolls by query
+router.post('/searchresults', async (req, res, next) => {
+  const { query } = req.body;
+  console.log(query)
+  try {
+    if (query !== '') {
+      const allDolls = await Doll.find();
+      const dolls = allDolls.filter((item) => {
+        return (
+          item.name.toLowerCase().includes(query.toLowerCase()) ||
+          item.character.toLowerCase().includes(query.toLowerCase()) ||
+          item.mold.toLowerCase().includes(query.toLowerCase()) ||
+          item.skinTone.toLowerCase().includes(query.toLowerCase()) ||
+          item.body.toLowerCase().includes(query.toLowerCase()) ||
+          item.hair.toLowerCase().includes(query.toLowerCase()) ||
+          item.subBrand.toLowerCase().includes(query.toLowerCase()) ||
+          item.collectionName.toLowerCase().includes(query.toLowerCase())
+        );
+      });
+      console.log(dolls)
+      res.json(dolls);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 module.exports = router;
